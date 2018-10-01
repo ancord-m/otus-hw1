0b999714b599a3cae764ff64f67d4ba038b84760
Michael Latushko <ancord.m@yandex.ru>
2018-10-01 11:36:10 +0300

    Added lib.h

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
-int main(void)
+#include <iostream>
+
+int main(int argc, char const *argv[])
 {
+	std::cout << "Version " << version() << std::endl;
+	std::cout << "Hello, World!" << std::endl;
+
 	return 0;
 }
