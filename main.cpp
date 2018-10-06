2af9e8e542a57835f256fc4d9c1ad5beb7478fb2
Michael Latushko <ancord.m@yandex.ru>
2018-10-06 15:19:15 +0300

    Corrected output message

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char const *argv[])
 {
-	std::cout << "Version " << version() << std::endl;
+	std::cout << "build " << version() << std::endl;
 	std::cout << "Hello, World!" << std::endl;
 
 	return 0;
